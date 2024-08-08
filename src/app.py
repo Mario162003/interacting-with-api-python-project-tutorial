acfca8a5cb8c1003ccb5cbc4654249bbe3ed3c8a
Mario162003 <macarce2003@gmail.com>
2024-08-08 11:11:34 +0200

    comm1

--- src/app.py
+++ src/app.py
@@ -2,6 +2,20 @@ import os
 import pandas as pd
 import seaborn as sns
 from dotenv import load_dotenv
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import spotify
+from spotipy.oauth2 import SpotifyClientCredentials
 
-# load the .env file variables
 load_dotenv()
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SECRET")
+
+
+
+client_credential_manager = SpotifyClientCredentials(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("CLIENT_SECRET")) 
+
+sp = spotipy.Spotify(client_credentials_manager=client_credential_manager)
+
+Kendrick_uri = '2YZyLoL8N0Wb9xBt1NhZWg'
+spotipy.Spotify(client_credentials_manager = client_id)
